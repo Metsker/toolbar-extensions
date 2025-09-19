a62c9f7a689482e609f806d24d9df3384da03fcd
Lev <66064249+Metsker@users.noreply.github.com>
2025-09-19 11:55:41 +0300

    Force custom elements to be unfocusable

--- Editor/ToolbarExtensionsInitializer.cs
+++ Editor/ToolbarExtensionsInitializer.cs
@@ -16,7 +16,11 @@ namespace ToolbarExtensions
         {
             StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Packages/com.metsker.toolbar-extensions/Editor/USS/ToolbarExtensions.uss");
             MainToolbar.UnityToolbarRoot.styleSheets.Add(styleSheet);
+
+            MainToolbar.CenterContainer.Q("ToolbarAutomaticExtenderLeftContainer").Query().ForEach(e => e.focusable = false);
+            MainToolbar.CenterContainer.Q("ToolbarAutomaticExtenderRightContainer").Query().ForEach(e => e.focusable = false);
         }
     }
 }
+
 
